d9b24f2ccc6f1ee6379cdb49f585745a61f608a0
igobb <t.golab06@gmail.com>
2023-04-01 19:40:28 +0200

    fix: fixed eslint configuration

--- src/pages/AddCartPage/AddCartPage.tsx
+++ src/pages/AddCartPage/AddCartPage.tsx
@@ -2,7 +2,9 @@ import "./AddCartPage.scss";
 
 const AddCartPage = () => {
     return ( <div className="add-cart-page__container">
-
+        <form>
+            <input type='text' name=""></input>
+        </form>
     </div> );
 }
  
