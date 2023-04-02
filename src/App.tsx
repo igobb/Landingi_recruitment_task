f026cea6ddf7a83f90e60b29ea027aa3a7558396
igobb <t.golab06@gmail.com>
2023-04-02 12:06:55 +0200

    feat: added errorBoundary to handle errors & added possibility to click on logo and go to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Suspense } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Routes from "./routes/Routes";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        {/* dodać komponent ładowania */}
-        <Routes />
-      </Suspense>
-    </div>
+    <ErrorBoundary>
+      <div className="App">
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          {/* dodać komponent ładowania */}
+          <Routes />
+        </Suspense>
+      </div>
+    </ErrorBoundary>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import "./Header.scss";
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
     return ( <header className="header__container">
             <div className="header__logo">
-                <p>SOME LOGO</p>
+                <p onClick={() => navigate("/")}>SOME LOGO</p>
             </div>
             <div className="header__navbar">
                 <Navbar/>
